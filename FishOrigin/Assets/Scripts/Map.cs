3b70cd6150752bfb0fdf2a1c3aa80e101fbd42c3
aaRookie <30520688+aaRookie@users.noreply.github.com>
2020-11-29 18:07:16 +0800

    关卡数据

--- FishOrigin/Assets/Scripts/Map.cs
+++ FishOrigin/Assets/Scripts/Map.cs
@@ -100,7 +100,7 @@ public class Map : MonoBehaviour
     void InitMap()
     {
         //LoadLevelData(SelectLevel.Instance.CurrentLevel);
-        LoadLevelData(4);
+        LoadLevelData(5);
 
         player_x = 0;
         player_y = 0;
