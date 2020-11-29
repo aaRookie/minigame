8c43c405b1a03155e012cd46d7ffeef86e147d9f
aaRookie <30520688+aaRookie@users.noreply.github.com>
2020-11-30 04:29:14 +0800

    初始场景动画

--- FishOrigin/Assets/Scripts/FirstUIController.cs
+++ FishOrigin/Assets/Scripts/FirstUIController.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
+using UnityEngine.SceneManagement;
 
 public class FirstUIController : MonoBehaviour
 {
 
-    public GameObject m_back;
     public GameObject m_white;
     public GameObject m_yellowback;
     public GameObject m_grass1;
@@ -20,6 +20,9 @@ public class FirstUIController : MonoBehaviour
     public GameObject m_tree3;
     public GameObject m_tree4;
     public GameObject m_tree5;
+    public GameObject m_logo;
+
+    public float TempTime = 0f;
     void Start()
     {
         
@@ -28,30 +31,48 @@ public class FirstUIController : MonoBehaviour
    
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        TempTime += Time.deltaTime;
+
+
+        if (Input.GetMouseButtonDown(0))
         {
-            if(m_back.transform.localScale==Vector3.one)
+            if(TempTime>6f)
             {
-                m_back.transform.DOScale(new Vector3(1.2f, 1.2f, 1.2f), 1f);
-                StartCoroutine(ShowWhite());
+                SceneManager.LoadScene(1);
+            }
+
+            if(m_white.transform.localScale==Vector3.one)
+            {
+                m_white.transform.DOScale(new Vector3(1.2f, 1.2f, 1.2f), 1f);
+                StartCoroutine(ShowImage());
             }
         }
     }
 
-    IEnumerator ShowWhite()
+    IEnumerator ShowImage()
     {
         yield return new WaitForSeconds(1f);
-        m_white.SetActive(true);
-        m_grass1.SetActive(true);
-        m_grass2.SetActive(true);
-        m_grass3.SetActive(true);
-        m_grass4.SetActive(true);
-        m_grass5.SetActive(true);
-        m_tree1.SetActive(true);
-        m_tree2.SetActive(true);
-        m_tree3.SetActive(true);
-        m_tree4.SetActive(true);
-        m_tree5.SetActive(true);
-        m_yellowback.SetActive(true);
+        m_yellowback.GetComponent<Image>().DOFade(1, 0.5f);
+        yield return new WaitForSeconds(0.8f);
+        m_grass4.GetComponent<Image>().DOFade(1, 0.5f);
+        yield return new WaitForSeconds(0.8f);
+        m_grass2.GetComponent<Image>().DOFade(1, 0.5f);      
+        yield return new WaitForSeconds(0.8f);
+        m_grass1.GetComponent<Image>().DOFade(1, 0.5f);
+        m_grass3.GetComponent<Image>().DOFade(1, 0.5f);
+        yield return new WaitForSeconds(0.8f);
+        m_tree3.GetComponent<Image>().DOFade(1, 0.5f);
+        m_tree5.GetComponent<Image>().DOFade(1, 0.5f);
+        m_tree1.GetComponent<Image>().DOFade(1, 0.5f);
+        yield return new WaitForSeconds(0.8f);
+        m_tree2.GetComponent<Image>().DOFade(1, 0.5f);
+        m_tree4.GetComponent<Image>().DOFade(1, 0.5f);
+        m_grass5.GetComponent<Image>().DOFade(1, 0.5f);
+        yield return new WaitForSeconds(0.5f);
+        m_logo.SetActive(true);
+
+        //yield return new WaitForSeconds(0.8f);
+        //m_grass5.GetComponent<Image>().DOFade(1, 0.5f);
+
     }
 }
